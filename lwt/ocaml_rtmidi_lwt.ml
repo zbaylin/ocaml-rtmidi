2e36c4422c2d4d011a7906ef4b4e4daa8614ced7
Zach Baylin <me@zachbayl.in>
2022-04-11 22:08:48 -0400

    Lwt: change poll time

--- lwt/ocaml_rtmidi_lwt.ml
+++ lwt/ocaml_rtmidi_lwt.ml
@@ -2,9 +2,9 @@ open Ocaml_rtmidi
 open Lwt.Infix
 
 module In = struct
-  let message_stream ?(wait_time_s = 0.01) in_ =
+  let message_stream ?(poll_time_s = 0.001) in_ =
     let rec helper () =
-      Lwt_unix.sleep wait_time_s >>= fun () ->
+      Lwt_unix.sleep poll_time_s >>= fun () ->
       let dt, data = In.get_message in_ in
       if dt > 0. then Lwt.return (Some (dt, data)) else helper ()
     in
